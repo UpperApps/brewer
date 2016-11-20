077a04be8e6a14374e5f39c09e50e0265a023357
UpperApps <upperapps@gmail.com>
2016-11-20 21:11:26 -0200

    Carregamento dinâmico dos combos e radio boxes com dados do modelo.

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -10,10 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
-import com.algaworks.brewer.repository.Cervejas;
+import com.algaworks.brewer.model.Origem;
+import com.algaworks.brewer.model.Sabor;
+import com.algaworks.brewer.repository.Estilos;
 
 @Controller
 public class CervejasController {
@@ -22,28 +25,29 @@ public class CervejasController {
 	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
 
 	@Autowired
-	private Cervejas cervejas;
-	
-	@RequestMapping("/cervejas/novo")
-	public String novo(Cerveja cerveja) {
-		cervejas.findAll();
+	private Estilos estilos;
 		
-		return "cerveja/CadastroCerveja";
+	@RequestMapping("/cervejas/novo")
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
+		
+		return mv;
 	}
 
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+
 		if (result.hasErrors()) {
-			// O Model deve ser utilizado com o Forward para retornar dados para
-			// a view.
+			// O Model deve ser utilizado com o Forward para retornar dados para a view.
 			return novo(cerveja);
 		}
 
-		// Para o Redirect deve ser utilizado o RedirectAttributes para retornar
-		// dados para a view.
+		// Para o Redirect deve ser utilizado o RedirectAttributes para retornar dados para a view.
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
-		System.out.println(">>> sku: " + cerveja.getSku());
-		return "redirect:/cervejas/novo";
+		return new ModelAndView("redirect:/cervejas/novo");
 	}
 	
 	@RequestMapping(value = "/cervejas/cadastro", method = RequestMethod.GET)
